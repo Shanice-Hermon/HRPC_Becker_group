60556bdc0b8cbce34bc548f6e47951548fcd342f
jarmsmagalang <21294515+jarmsmagalang@users.noreply.github.com>
2026-02-13 15:22:49 +0100

    Convert modification names to MODOMICS names

--- bedrmod_convert.py
+++ bedrmod_convert.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Feb 12 21:02:19 2026
-
-@author: jm21p335
-"""
-
 import pandas as pd
 
 NON_MOD_BASES = ["pAr", "pUr", "pGr", "pCr"]
@@ -13,9 +6,9 @@ def find_mod_pos(row):
     oligo_seq_list = ("p"+row).split("-")[:-1]
     return [(x,i) for i, x in enumerate(oligo_seq_list) if x not in NON_MOD_BASES]
 
-
-def generate_bedrmod(filename):
+def generate_bedrmod(filename, filename_mapping):
     df = pd.read_csv(filename)
+    df_mapping = pd.read_csv(filename_mapping, sep = "\t")
     
     df = df[(df["Best ASR"] <= 1) & (df["Conf. Score"] >= 100)]
     
@@ -27,7 +20,7 @@ def generate_bedrmod(filename):
     df2 = df[df["mod"].map(len)>0][["pos_start", "mod", "Conf. Score"]]
     
     df2 = df2.explode("mod")
-    df2["name"] = df2["mod"].apply(lambda x: x[0])
+    df2["mod_name"] = df2["mod"].apply(lambda x: x[0])
     df2["mod_idx"] = df2["mod"].apply(lambda x: x[1])
     
     df2["chromEnd"] = df2["pos_start"]+df2["mod_idx"]
@@ -35,22 +28,22 @@ def generate_bedrmod(filename):
     
     df2 = df2.drop(["mod", "pos_start", "mod_idx"], axis = 1)
     
-    
-    df3 = df2.groupby(["name", 
+    df3 = df2.groupby(["mod_name", 
                        "chromStart", 
-                       "chromEnd"]).agg(coverage = ("name", "size"),
+                       "chromEnd"]).agg(coverage = ("mod_name", "size"),
                                         score = ("Conf. Score", "mean")).reset_index()
     df3["frequency"] = (df3["coverage"]/df.shape[0])*100
+    
+    df3 = pd.merge(df3, df_mapping, how = "left", left_on = "chromEnd", right_on = "pos")
+    df3 = df3.drop(["mod_name", "pos"], axis = 1)
     
     df3["thickStart"] = df3["chromStart"]
     df3["thickEnd"] = df3["chromEnd"]
     
-    df3["chrom"] = filename.split("_")[1]
+    df3["chrom"] = filename.split("_")[2]
     df3["strand"] = "."
     df3["itemRgb"] = 0
-    
-    # df3["name"] = df3["name"].apply(lambda x: x[1:])
-    
+       
     df_final = df3.loc[:, ["chrom",
                            "chromStart",
                            "chromEnd",
@@ -61,17 +54,24 @@ def generate_bedrmod(filename):
                            "thickEnd",
                            "itemRgb",
                            "coverage",
-                           "frequency"]].sort_values(by = ["chromStart"]).reset_index(drop = True)
+                           "frequency"]
+                       ].sort_values(by = ["chromStart"]).reset_index(drop = True)
     
     return df_final
 
 files = [
-         "HRPC_5.8S_rRNA.csv",
-         "HRPC_18S_rRNA_02.csv",
-         "HRPC_28S_rRNA.csv"
+         "bpf_mapping/HRPC_5.8S_rRNA.csv",
+         "bpf_mapping/HRPC_18S_rRNA_02.csv",
+         "bpf_mapping/HRPC_28S_rRNA.csv"
          ]
 
-dfs = [generate_bedrmod(f) for f in files]
+mapping_files = [
+                "bpf_mapping/update_meta_5.8S.tsv",
+                "bpf_mapping/update_meta_18S.tsv",
+                "bpf_mapping/update_meta_28S.tsv"
+                ]
+
+dfs = [generate_bedrmod(files[i], mapping_files[i]) for i in range(len(files))]
 
 df_final = pd.concat(dfs).reset_index(drop = True)
 
